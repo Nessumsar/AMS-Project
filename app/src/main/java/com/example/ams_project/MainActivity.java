5e85514abfecd279bff1d1086474524fb4334ccc
Lukas Rasmussen <lukasrasmusseen@gmail.com>
2020-06-22 09:22:48 +0200

    API Button ID changed

--- app/src/main/java/com/example/ams_project/MainActivity.java
+++ app/src/main/java/com/example/ams_project/MainActivity.java
@@ -6,11 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button compareBtn,assignBtn;
+    private Button compareBtn, apiBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         compareBtn = findViewById(R.id.main_compare_btn);
-        assignBtn = findViewById(R.id.main_assign_btn);
+        apiBtn = findViewById(R.id.main_api_btn);
 
 
         compareBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(compareIntent);
             }
         });
-        assignBtn.setOnClickListener(new View.OnClickListener() {
+        apiBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent assignIntent = new Intent(MainActivity.this, AssignmentActivity.class);
+                Intent assignIntent = new Intent(MainActivity.this, ApiActivity.class);
                 startActivity(assignIntent);
             }
         });
